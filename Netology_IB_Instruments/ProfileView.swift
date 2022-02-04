7302ade2353f48271a6375980f6306d141c882ae
zalik198 <zshomakhov@mail.ru>
2022-02-04 04:25:05 +0300

    homework 1.2

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Shom on 03.02.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var labelBirthday: UILabel!
+    @IBOutlet weak var labelLocation: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Shom on 03.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
